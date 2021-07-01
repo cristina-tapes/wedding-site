4a52a06865e4648dc3ebbbe16ff8f878a9706db1
Cristina Tapes <cristina.v.tapes@gmail.com>
2021-07-01 15:55:46 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 import axios from "axios";
 import Home from "./components/home";
 import { About } from "./components/about";
-import { Contact } from "./components/rsvp";
+import { Rsvp } from "./components/rsvp";
 import { Footer } from "./components/footer";
+import { Invitation } from "./components/invitation";
+import { Events } from "./components/events";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-
-import invitationContent from "./data.json";
 
 export enum Languages {
   en = "en",
@@ -19,6 +19,34 @@ interface IUser {
   language: Languages;
   greeting: string;
 }
+
+const FooterTransitionMobile: React.FunctionComponent<{}> = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="footerTransitionMobile"
+      viewBox="0 0 1440 320"
+    >
+  <path fill="#7DA58C" fill-opacity="1" d="M0,128L80,122.7C160,117,320,107,480,112C640,117,800,139,960,133.3C1120,128,1280,96,1360,80L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+</svg>
+  );
+};
+
+const FooterTransitionDesktop: React.FunctionComponent<{}> = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="footerTransition"
+      viewBox="0 0 1440 320"
+    >
+      <path
+        fill="#7DA58C"
+        fill-opacity="1"
+        d="M0,224L80,234.7C160,245,320,267,480,266.7C640,267,800,245,960,240C1120,235,1280,245,1360,250.7L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+      ></path>
+    </svg>
+  );
+};
 
 const AppInternal: React.FunctionComponent<{}> = () => {
   const queryParams = new URLSearchParams(window.location.search);
@@ -35,8 +63,11 @@ const AppInternal: React.FunctionComponent<{}> = () => {
   const { data, error, isLoading } = useQuery("user", () => fetchUser(id));
 
   console.log(JSON.stringify(data), error, isLoading);
-  const [language, setLanguage] = React.useState<Languages>(lang || Languages.ro);
-  const [content] = React.useState(invitationContent);
+  // eslint-disable-next-line
+  const [language, setLanguage] = React.useState<Languages>(
+    lang || Languages.ro
+  );
+  // eslint-disable-next-line
   const [invitationRsvp, setRsvp] = React.useState<IUser>(data);
 
   React.useEffect(() => {
@@ -48,9 +79,13 @@ const AppInternal: React.FunctionComponent<{}> = () => {
   return (
     <div className="App">
       <Home language={language} />
-      <About data={content} />
-      <Contact data={content} />
-      <Footer data={content} />
+      <About language={language} />
+      <Invitation language={language} />
+      <Events language={language} />
+      <Rsvp language={language} />
+      <FooterTransitionDesktop />
+      <FooterTransitionMobile />
+      <Footer language={language} />
     </div>
   );
 };
