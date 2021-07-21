df6966a764e1ecc729721c785ccd63e43e7c02e1
Cristina Tapes <cristina.tapes@itiviti.com>
2021-07-21 12:04:01 +0300

    fix russian invitation

--- src/components/invitation.tsx
+++ src/components/invitation.tsx
@@ -22,51 +22,75 @@ export const Invitation: React.FunctionComponent<{
     cristinaSpiritualParents,
     willSay,
     yes,
-    confirmMessage,
+    confirmMessage
   } = t[language];
 
   return (
     <section id="invitation">
       <img className="divider transform" src={divider} alt="" />
       <h1 className="title">{greeting ? greeting : genericGreeting}</h1>
-      <div>
-        <h3>{!greeting || plural ? seeYouPl : seeYou}</h3>
-      </div>
-      <div className="invitationDate">
-        <h3>{date}</h3>
-      </div>
-      <div>
-        <h3>{inFrontOfParents}</h3>
-      </div>
-      <div className="parents">
-        <div className="invitationParent">
-          <h3>{alexParents}</h3>
-        </div>
-        <div className="connector">
-          <h4>{connector}</h4>
-        </div>
-        <div className="invitationParent">
-          <h3>{cristinaParents}</h3>
-        </div>
-      </div>
-      <div>
-        <h3>{andSpiritualParents}</h3>
-      </div>
-      <div className="spiritualParents">
-        <div className="invitationSpiritualParent">
-          <h3>{alexSpiritualParents}</h3>
-        </div>
-        <div className="connector">
-          <h4>{connector}</h4>
-        </div>
-        <div className="invitationSpiritualParent">
-          <h3>{cristinaSpiritualParents}</h3>
-        </div>
-      </div>
-      <div>
-        <h3>{willSay}</h3>
-      </div>
-      <h1>{yes}</h1>
+
+      {language !== Languages.ru && (
+        <>
+          <div>
+            <h3>{!greeting || plural ? seeYouPl : seeYou}</h3>
+          </div>
+          <div className="invitationDate">
+            <h3>{date}</h3>
+          </div>
+          <div>
+            <h3>{inFrontOfParents}</h3>
+          </div>
+          <div className="parents">
+            <div className="invitationParent">
+              <h3>{alexParents}</h3>
+            </div>
+            <div className="connector">
+              <h4>{connector}</h4>
+            </div>
+            <div className="invitationParent">
+              <h3>{cristinaParents}</h3>
+            </div>
+          </div>
+          <div>
+            <h3>{andSpiritualParents}</h3>
+          </div>
+          <div className="spiritualParents">
+            <div className="invitationSpiritualParent">
+              <h3>{alexSpiritualParents}</h3>
+            </div>
+            <div className="connector">
+              <h4>{connector}</h4>
+            </div>
+            <div className="invitationSpiritualParent">
+              <h3>{cristinaSpiritualParents}</h3>
+            </div>
+          </div>
+          <div>
+            <h3>{willSay}</h3>
+          </div>
+          <h1>{yes}</h1>{" "}
+        </>
+      )}
+      {language === Languages.ru && (
+        <>
+          <div>
+            <h3>{!greeting || plural ? seeYouPl : seeYou}</h3>
+          </div> 
+          <div>
+            <h3>{inFrontOfParents}</h3>
+          </div>
+          <div className="invitationDate">
+            <h3>{date}</h3>
+          </div>
+          <div>
+            <h3>{willSay}</h3>
+          </div>
+          <div>
+            <h3>{yes}</h3>
+          </div>
+        </>
+      )}
       <p>{confirmMessage}</p>
     </section>
   );
